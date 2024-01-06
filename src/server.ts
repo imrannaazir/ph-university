fc7140e463d994476a1a05e7581c714501a8c742
emon <imrannaaziremon@gmail.com>
2024-01-06 22:32:02 +0600

    [HANDLED] - 'uncaughtException & unhandledRejection

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
+
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
 import colors from 'colors';
 
+let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(colors.green.bold(`App is listing on port: ${config.port}`));
     });
   } catch (error) {
@@ -16,3 +19,20 @@ async function main() {
 }
 
 main();
+
+// handle unhandledRejection
+process.on('unhandledRejection', () => {
+  console.log(`😈 unhandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//handle uncaughtException
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
