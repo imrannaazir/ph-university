e7a82aae323cf478b0c63cc62234bca97cdbbcf2
emon <imrannaaziremon@gmail.com>
2024-01-07 02:56:05 +0600

    [CREATED] - validation schema for faculty

---
+++ src/app/modules/faculty/faculty.constant.ts
@@ -0,0 +1,12 @@
+export const FacultyGender = ['Male', 'Female', 'Other'];
+
+export const BloodGroups: string[] = [
+  'A+',
+  'A-',
+  'B+',
+  'B-',
+  'AB+',
+  'AB-',
+  'O+',
+  'O-',
+];
---
+++ src/app/modules/faculty/faculty.validation.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import { BloodGroups, FacultyGender } from './faculty.constant';
+
+const facultyNameValidationSchema = z.object({
+  firstName: z.string(),
+  middleName: z.string().optional(),
+  lastName: z.string(),
+});
+
+export const createFacultyValidationSchema = z.object({
+  body: z.object({
+    id: z.string(),
+    name: facultyNameValidationSchema,
+    contactNo: z.string(),
+    academicDepartment: z.string(),
+    designation: z.string(),
+    isDeleted: z.boolean(),
+    permanentAddress: z.string(),
+    presentAddress: z.string(),
+    user: z.string(),
+    dateOfBirth: z.date().optional(),
+    profileImage: z.string().optional(),
+    gender: z.enum([...FacultyGender] as [string, ...string[]]),
+    bloodGroup: z.enum([...BloodGroups] as [string, ...string[]]),
+  }),
+});
