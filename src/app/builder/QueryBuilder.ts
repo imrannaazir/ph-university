7a2a7c91966277b987c8965e5eefbea7677ff9fd
emon <imrannaaziremon@gmail.com>
2024-01-07 01:44:24 +0600

    [CREATED] - queryBuilder for filtering , sorting, paginationg etc

---
+++ src/app/builder/QueryBuilder.ts
@@ -0,0 +1,70 @@
+import { FilterQuery, Query } from 'mongoose';
+
+class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+
+  // searching method
+  search(searchableFields: string[]) {
+    const searchTerm = this?.query?.searchTerm;
+    this.modelQuery = this.modelQuery.find({
+      $or: searchableFields.map(
+        (field: string) =>
+          ({
+            [field]: { $regex: searchTerm, options: 'i' },
+          } as FilterQuery<T>)
+      ),
+    });
+
+    return this;
+  }
+
+  //filtering method
+  filter() {
+    const queryObj = {
+      ...this.query,
+    };
+    const excludedFields = ['limit', 'page', 'searchTerm', 'fields', 'sort'];
+
+    excludedFields.forEach((field) => delete queryObj[field]);
+
+    this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
+    return this;
+  }
+
+  //sorting method
+  sort() {
+    const sort =
+      (this?.query?.sort as string).split(',').join(' ') || '-createdAt';
+
+    this.modelQuery = this.modelQuery.find().sort(sort);
+    return sort;
+  }
+
+  //fields selecting method
+  fields() {
+    const fields =
+      (this?.query?.fields as string).split(',').join(' ') || '-__v';
+
+    this.modelQuery = this.modelQuery.find().select(fields);
+    return this;
+  }
+
+  // paginating method
+  paginate() {
+    const page = Number(this?.query?.page);
+    const limit = Number(this?.query?.limit);
+    const skip = (page - 1) * limit;
+
+    this.modelQuery = this.modelQuery.find().skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+export default QueryBuilder;
