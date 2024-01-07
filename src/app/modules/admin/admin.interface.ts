ede197e9fdf7530472bad83c1e1e3cc73b7db389
emon <imrannaaziremon@gmail.com>
2024-01-07 12:17:49 +0600

    [CREATED] - interface for admin

---
+++ src/app/modules/admin/admin.interface.ts
@@ -0,0 +1,34 @@
+import { Types } from 'mongoose';
+
+export type TAdminName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+export type TGender = 'Male' | 'Female' | 'Other';
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TAdmin = {
+  id: string;
+  user: Types.ObjectId;
+  name: TAdminName;
+  designation: string;
+  gender: TGender;
+  email: string;
+  dateOfBirth?: Date;
+  contactNo: string;
+  emergencyContactNo?: string;
+  bloodGroup: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImage?: string;
+  isDeleted?: boolean;
+};
