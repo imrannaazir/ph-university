ac37cad09a678ff6c3a7632b26d4bb22975749de
emon <imrannaaziremon@gmail.com>
2024-02-15 12:11:09 +0600

    [CREATED] - auth api and added login mutation

--- client/src/redux/api/baseApi.ts
+++ client/src/redux/api/baseApi.ts
@@ -1,11 +1,11 @@
 // Or from '@reduxjs/toolkit/query/react'
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseQuery = createApi({
+export const baseApi = createApi({
   reducerPath: "api",
   // Set the baseUrl for every endpoint below
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
   endpoints: () => ({}),
 });
 
-export default baseQuery;
+export default baseApi;
