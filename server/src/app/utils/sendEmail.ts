f7869576bc56c94ccf22e8b0234638ac3fa8e19a
emon <imrannaaziremon@gmail.com>
2024-03-11 21:22:12 +0600

    [UPDATED] - send email file

--- server/src/app/utils/sendEmail.ts
+++ server/src/app/utils/sendEmail.ts
@@ -1,21 +1,21 @@
-import nodemailer from 'nodemailer';
-import config from '../config';
-import { TEmailPayload } from '../interface/interface';
+import nodemailer from 'nodemailer'
+import config from '../config'
+import { TEmailPayload } from '../interface/interface'
 
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true,
+  secure: config.NODE_ENV === 'production',
   auth: {
     user: config.my_email_address,
     pass: config.email_app_password,
   },
-});
+})
 
 // utility for send email
 const sendEmail = async (payload: TEmailPayload) => {
-  const { receiver, html, subject, text } = payload;
+  const { receiver, html, subject, text } = payload
   try {
     const info = await transporter.sendMail({
       from: config.my_email_address, // sender address
@@ -23,13 +23,14 @@ const sendEmail = async (payload: TEmailPayload) => {
       subject: subject, // Subject line
       text: text, // plain text body
       html: html, // html body
-    });
+    })
 
-    console.log(info.messageId);
+    // eslint-disable-next-line no-console
+    console.log(info.messageId)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-    throw new Error(error);
+    throw new Error(error)
   }
-};
+}
 
-export default sendEmail;
+export default sendEmail
